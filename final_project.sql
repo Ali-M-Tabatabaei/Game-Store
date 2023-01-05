48bf41ed0950053b0a0d0e76ffe6302d9385708d
Ali Mohammad Tabatabaei <85901084+Ali-M-Tabatabaei@users.noreply.github.com>
2023-01-05 12:57:24 +0330

    Delete final_project.sql

--- final_project.sql
+++
@@ -1,9 +0,0 @@
-#create database final_project
-use final_project
-/*CREATE TABLE IF NOT EXISTS company (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) ,
-  `city` VARCHAR(45) ,
-  PRIMARY KEY (`id`))
-*/
-show TABLES
